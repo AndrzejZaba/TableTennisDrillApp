3af4abd98a8c8b553b0dd9d977dbaad422aba277
AndrzejZaba <andrzejzaba@onet.pl>
2023-05-02 18:17:43 +0200

    Added description to DrillContentViewModel

--- TableTennisDrillApp/ViewModels/DrillContentViewModels/DrillContentViewModel.cs
+++ TableTennisDrillApp/ViewModels/DrillContentViewModels/DrillContentViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 using TableTennisDrillApp.Models;
 
 namespace TableTennisDrillApp.ViewModels.DrillContentViewModels
@@ -13,13 +15,47 @@ namespace TableTennisDrillApp.ViewModels.DrillContentViewModels
 
         public List<string> Images => _drill.Images.Select(r => string.Concat(@"/TableTennisDrillApp;component/Images/", r)).ToList();
         public string ImageOne => Images[0];
-        public List<string> Description => _drill.Description;
+        public ICollection<DescriptionLine> DrillDescription { get; set; }
         public TimeSpan DurationTime => _drill.DurationTime;
 
         public DrillContentViewModel(Drill drill)
         {
             _drill = drill;
+            DrillDescription = new ObservableCollection<DescriptionLine>();
+            foreach (var line in _drill.Description)
+            {
+                if (line.First().Equals('A'))
+                {
+                    DrillDescription.Add(new DescriptionLine(line.Remove(0,2), @"/TableTennisDrillApp;component/Icons/A_PlayerM.png"));
+                }
+                else
+                {
+                    DrillDescription.Add(new DescriptionLine(line.Remove(0,2), @"/TableTennisDrillApp;component/Icons/B_PlayerM.png"));
+                }
+
+            }
         }
 
+        public DrillContentViewModel()
+        {
+            DrillDescription = new ObservableCollection<DescriptionLine>
+            {
+                new DescriptionLine("Test1", @"/TableTennisDrillApp;component/Icons/A_PlayerM.png"),
+                new DescriptionLine("Test2", @"/TableTennisDrillApp;component/Icons/A_PlayerM.png"),
+                new DescriptionLine("Test3", @"/TableTennisDrillApp;component/Icons/A_PlayerM.png")
+            };
+        }
+
+    }
+
+    public class DescriptionLine
+    {
+        public string Description { get; set; }
+        public string PlayerIcon { get; set; }
+        public DescriptionLine(string description, string playerIcon)
+        {
+            Description = description;
+            PlayerIcon = playerIcon;
+        }
     }
 }
